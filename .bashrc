d87ededb01a40859e66bb97f1931b3504b6e56cd
yamagata <rustyarchive@gmail.com>
2013-12-12 10:59:33 +0900

    .bashrcの追加

---
+++ .bashrc
@@ -0,0 +1,20 @@
+
+source ~/.git-prompt.sh
+source ~/.git-completion.bash
+
+GIT_PS1_SHOWDIRTYSTATE=true
+PS1="\[\033[00m\][\[\033[32m\]\u@\h \[\033[34m\]\w\[\033[00m\]]\[\033[31m\]\$(__git_ps1)\[\033[00m\] \$ "
+HISTTIMEFORMAT='%Y-%m-%d %T '
+CLICOLOR=1
+LSCOLOR=gxfxcxdxbxegedabagacad
+
+if [ ${WWW_HOME-UNDEF} = 'UNDEF' ]; then
+    export WWW_HOME='google.co.jp'
+fi
+
+alias ls='ls -G'
+alias ll='ls -l -h'
+alias la='ls -a'
+alias grep='grep --color'
+# alias ctags='/opt/local/bin/ctags'
+
--- setup.bash
+++ setup.bash
@@ -11,7 +11,12 @@ if ! type vim > /dev/null 2>&1; then
     exit 1
 fi
 
-ignore_files=". .. .git .vim setup.bash README.md .gitmodules"
+if ! type wget > /dev/null 2>&1; then
+    echo "wget is nothing."
+    exit 1
+fi
+
+ignore_files=". .. .git .vim setup.bash README.md .gitmodules .bashrc"
 
 current_path=`pwd`
 target_files=`ls -1a | tr '\n' ' '`
@@ -41,6 +46,11 @@ vim -c "NeoBundleInstall" -c q!
 
 echo "make synbolic link: $current_path/.vim/userautoload -> ~/.vim/userautoload"
 ln -s $current_path/.vim/userautoload ~/.vim/userautoload
+
+wget -O ~/.git-completion.bash --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+wget -O ~/.git-prompt.sh --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+cat .bashrc >> ~/.bashrc
+source ~/.bashrc
 
 git submodule init
 git submodule update
