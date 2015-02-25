5fe6f96922562d4e3338702158564f771381081b
yamagata_yu <yamagata_yu@kmktg04.com>
2015-02-25 15:28:24 +0900

    update setup script.

--- setup.bash
+++ setup.bash
@@ -1,65 +1,55 @@
 #!/usr/bin/env bash
 
-GIT=`which git 2> /dev/null`
-VIM=`which vim 2> /dev/null`
-WGET=`which wget 2> /dev/null`
+set -eu
 
-if test "-$GIT-" = "--" || test "-$VIM-" = "--" || test "-$WGET-" = "--"
-then
+curpath=$(cd $(dirname $0); pwd)
+ignores=`cat $curpath/setup.ignore`
+
+is_ignore() {
+    file=$1
+    for i in $ignores; do
+        if [ $i = $file ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+_git=`which git 2> /dev/null`; _vim=`which vim 2> /dev/null`; _wget=`which wget 2> /dev/null`
+if [ -z "${_git}" ] || [ -z "${_vim}" ] || [ -z "${_wget}" ]; then
     echo You must install git, vim, wget.
     exit 1
 fi
 
-current_path=$(cd $(dirname $0); pwd)
+targets=`find . -maxdepth 1 -name '\.*' | grep -vE '^$' | grep -vE '^\.+$' | sed -e 's/^\.*\/\(.*\)/\1/g'`
+for f in $targets; do
+    target=$curpath/$f
 
-ignore_files=`cat $current_path/setup.ignore`
-target_files=`ls -1a --color=none | grep -vE "^\.+$"`
-
-for file in $target_files
-do
-    is_ignore=1
-    target=$current_path/$file
-
-    if test -d $target
-    then
-        echo $target is directory.
-        is_ignore=0
+    if [ -d $target ]; then
+        echo "${target} is directory."
+        continue
     fi
-
-    for ignore_file in $ignore_files
-    do
-        if test $file = $ignore_file
-        then
-            echo ignore $file
-            is_ignore=0
-            break
-        fi
-    done
-
-    if test $is_ignore -eq 0
-    then
+    if is_ignore $f; then
+        echo "${target} is ignored."
         continue
     fi
 
-    echo synbolic link: $target to ~/$file
-    ln -s $target ~/$file
+    echo "cat ${target} >> ~/${f}"
+    cat $target >> ~/$f
 done
 
 git clone git@github.com:Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim/
-vim -c "NeoBundleInstall" -c q!
+vim -c 'NeoBundleInstall' -c q!
 
-echo synbolic link: $current_path/.vim/ftplugin to ~/.vim/ftplugin
-ln -s $current_path/.vim/ftplugin ~/.vim/ftplugin
-echo synbolic link: $current_path/.vim/autoload to ~/.vim/autoload
-ln -s $current_path/.vim/autoload ~/.vim/autoload
-echo synbolic link: $current_path/.vim/userautoload to ~/.vim/userautoload
-ln -s $current_path/.vim/userautoload ~/.vim/userautoload
+echo "ln -s ${curpath}/.vim/ftplugin to ~/.vim/ftplugin"
+ln -s $curpath/.vim/ftplugin ~/.vim/ftplugin
+echo "ln -s ${curpath}/.vim/autoload to ~/.vim/autoload"
+ln -s $curpath/.vim/autoload ~/.vim/autoload
+echo "ln -s ${curpath}/.vim/userautoload to ~/.vim/userautoload"
+ln -s $curpath/.vim/userautoload ~/.vim/userautoload
 
 wget -O ~/.git-completion.bash --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
 wget -O ~/.git-prompt.sh --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
 
 git clone git@github.com:zsh-users/zsh-completions.git ~/.zsh-completions/
-
-cat .bashrc >> ~/.bashrc
-cat .zshrc >> ~/.zshrc
 
