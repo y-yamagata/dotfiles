288462a59cfc9a5677ff187647bfe81c0b5d38e2
yamagata <rustyarchive@gmail.com>
2017-07-15 19:08:19 +0900

    update.

--- setup.bash
+++ setup.bash
@@ -2,40 +2,68 @@
 
 set -eu
 
-curpath=$(cd $(dirname $0); pwd)
-ignores=`cat $curpath/setup.ignore`
+readonly curpath=$(cd $(dirname $0); pwd)
 
-is_ignore() {
-    file=$1
-    for i in $ignores; do
-        if [ $i = $file ]; then
+readonly ignore_file=$curpath/setup.ignore
+readonly lock_file=$curpath/setup.lock
+
+ignores=`cat ${ignore_file}`
+if -f $lock_file; then
+    locks=`cat ${}`
+fi
+
+function in_array {
+    local needle=$1
+    local hystack=$2
+
+    if [ -z "${hystack}" ]; then
+        return 1
+    fi
+
+    for i in $hystack; do
+        if [ $i = $needle ]; then
             return 0
         fi
     done
+
     return 1
+}
+
+function is_ignore {
+    is_array $1 $ignores
+}
+
+function is_locked {
+    is_array $1 $locks
 }
 
 _git=`which git 2> /dev/null`; _vim=`which vim 2> /dev/null`; _wget=`which wget 2> /dev/null`; _make=`which make 2> /dev/null`
 if [ -z "${_git}" ] || [ -z "${_vim}" ] || [ -z "${_wget}" ] || [ -z "${_make}" ] ; then
-    echo You must install git, vim, wget, make.
+    echo You must install git, vim, wget, make. >&2
     exit 1
 fi
 
-targets=`find . -maxdepth 1 -name '\.*' | grep -vE '^$' | grep -vE '^\.+$' | sed -e 's/^\.*\/\(.*\)/\1/g'`
-for f in $targets; do
-    target=$curpath/$f
+target_files=`find . -maxdepth 1 -name '\.*' | grep -vE '^$' | grep -vE '^\.+$' | sed -e 's/^\.*\/\(.*\)/\1/g'`
+for file in $target_files; do
+    filepath=$curpath/$file
 
-    if [ -d $target ]; then
-        echo "${target} is directory."
+    if [ -d $filepath ]; then
+        echo "${filepath} is directory."
         continue
     fi
-    if is_ignore $f; then
-        echo "${target} is ignored."
+    if is_ignore $file; then
+        echo "${filepath} is ignored."
+        continue
+    fi
+    if is_lock $file; then
+        echo "${filepath} is locked."
         continue
     fi
 
-    echo "cat ${target} >> ~/${f}"
-    cat $target >> ~/$f
+    echo "cat ${filepath} >> ~/${file}"
+    cat $filepath >> ~/$file
+
+    echo "${filepath}" >> $curpath/setup.lock
 done
 
 # vim settings
@@ -61,4 +89,3 @@ wget -O ~/.git-prompt.sh --no-check-certificate https://raw.github.com/git/git/m
 
 # zsh settings
 git clone git@github.com:zsh-users/zsh-completions.git ~/.zsh-completions/
-
